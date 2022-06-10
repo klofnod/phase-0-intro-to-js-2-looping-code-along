fceed161cd9fe45ec38d054272f7bfa69f732455
Jeremy <jeremy__francis@hotmail.com>
2022-06-10 09:49:13 -0700

    commit tree 69273951517121ae8c86d749c22f5647a2f2f160

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(list , event){
+    let message = []
+    for(let n=0; n < list.length; n++){
+        message.push (`Thank you, ${list[n]}, for the wonderful ${event} gift!`)
+    }   
+    return message
+}
+function countDown(num){
+    while (num >=0){
+        console.log(num)
+        num--
+    }
+}
